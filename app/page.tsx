ab95fcaeb7b1d1a76c342e652c01c99a4ef5bd07
emrahsariboz <emrahsariboz@certik.com>
2024-02-18 19:07:53 -0600

    riskylingo

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,38 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL } from './config';
 
-/*
-  This is the starting frame to begin the game
-
-*/
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      action: 'post',
-      label: 'Start Learning',
+      label: 'Story time!',
+    },
+    {
+      action: 'link',
+      label: 'Link to Google',
+      target: 'https://www.google.com',
+    },
+    {
+      label: 'Redirect to pictures',
+      action: 'post_redirect',
     },
   ],
   image: {
     src: `${NEXT_PUBLIC_URL}/risklingo.png`,
-    aspectRatio: '1.91:1',
+    aspectRatio: '1:1',
   },
-  postUrl: `${NEXT_PUBLIC_URL}/api/stakeFrame`,
+  input: {
+    text: 'Tell me a boat story',
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
-  title: 'Darian.xyz',
+  title: 'emrah.xyz',
   description: 'LFG',
   openGraph: {
-    title: 'Darian.xyz',
+    title: 'emrah.xyz',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/risklingo.png`],
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
   other: {
     ...frameMetadata,
@@ -36,7 +43,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>hello 123</h1>
+      <h1>emrah</h1>
     </>
   );
 }
